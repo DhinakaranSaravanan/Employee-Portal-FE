72f4c2724601c856024dd8d11517c2a737166d28
DhinakaranSaravanan <dhinakarannano@gmail.com>
2023-11-27 11:04:09 +0530

    url update

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 import { setCredentials} from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({    
-    baseUrl:'https://dk-employee-portal.onrender.com',
+    baseUrl:'https://dk-employee-portal-api.onrender.com',
     /* baseUrl:'http://localhost:3500', */
     credentials : 'include',
     prepareHeaders : (headers, {getState}) => {
